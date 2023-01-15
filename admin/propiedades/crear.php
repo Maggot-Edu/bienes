fc62b8827b44af4234ccb3b5313cf658a159abf9
Eduard <eduardbadoyan@gmail.com>
2023-01-15 12:40:39 +0100

    formulario crear propiedad

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -3,11 +3,52 @@
     incluirTemplate( 'header' );
 ?>
 
-    <main class="contenedor">
+    <main class="contenedor seccion contenido-centrado">
         <h1>Crear</h1>
 
-        <a href="/admin" class="boton boton-verde">Volver
-        </a>
+        <a href="/admin" class="boton boton-verde">Volver</a>
+        <br>
+        <form action="" class="formulario">
+            <fieldset>
+                <legend>Información General</legend>
+
+                <label for="titulo">Titulo:</label>
+                <input type="text" id="titulo" placeholder="Titulo Propiedad">
+
+                <label for="precio">Precio:</label>
+                <input type="number" id="precio" placeholder="Precio Propiedad">
+
+                <label for="imagen">Imagen:</label>
+                <input type="file" id="imagen" accept="image/jpeg, image/png">
+
+                <label for="descripcion">Descripción:</label>
+                <textarea id="descripcion" ></textarea>
+            </fieldset>
+
+            <fieldset>
+                <legend>Información Propiedad</legend>
+
+                <label for="habitaciones">Habitaciones:</label>
+                <input type="number" id="habitaciones" placeholder="Ej: 3" min="1" max="9">
+
+                <label for="wc">Baños:</label>
+                <input type="number" id="wc" placeholder="Ej: 3" min="1" max="9">
+
+                <label for="parking">Parking:</label>
+                <input type="number" id="parking" placeholder="Ej: 3" min="1" max="9">
+            </fieldset>
+
+            <fieldset>
+                <legend>Vendedor</legend>
+
+                <select>
+                    <option value="1">Eduard</option>
+                    <option value="2">No-name</option>
+                </select>
+            </fieldset>
+            <input type="submit" value="Crear Propiedad" class="boton boton-verde">
+        </form>
+
     </main>
 
 
