07d03a5014889abd96da012c0913fef6b6e7a273
Eduard <eduardbadoyan@gmail.com>
2023-01-29 16:31:41 +0100

    update

--- includes/config/ddbb.php
+++ includes/config/ddbb.php
@@ -1,7 +1,7 @@
 <?php
 
 function conexionDB() : mysqli {
-    $db = mysqli_connect('localhost', 'root', '', 'bienes_crud');
+    $db = mysqli_connect('localhost', 'root', 'root', 'bienes_crud');
 
     if (!$db) {
         echo "No se ha podido conectar a BBDD";
